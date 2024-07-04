397bd312bb54ceafc279d818daaacb234e6f5ead
kirillsaint <kirillsaint@wtfuck.tk>
2024-07-04 23:58:14 +0600

    better naming

--- src/translation/ru.ts
+++ src/translation/ru.ts
@@ -204,7 +204,7 @@ const ruTranslation = {
 	"Multi-Use %symbol% Check": "%symbol% Мульти-чек",
 	"Offers not found": "Объявления не найдены",
 	"But you can create your own offer :)": "Но вы можете создать свое :)",
-	"Referral Program": "Реферальная программа",
+	"Referral Program": "Реферальная система",
 	"Invite users and receive 30% of their commission.":
 		"Приглашайте пользоватателей и получайте 30% с их комиссии.",
 	"Your invite link": "Ваша реферальная ссылка",
