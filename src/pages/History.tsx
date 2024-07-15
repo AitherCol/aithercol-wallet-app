7d99d2faefa18b7af4ae2b09c1ffd8c3233a6f15
kirillsaint <kirillsaint@wtfuck.tk>
2024-07-15 11:08:13 +0600

    fix

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -144,6 +144,7 @@ function History({ hideBackButton }: { hideBackButton?: boolean }) {
 				errorHandler(error, toast);
 				notificationOccurred("error");
 			}
+			window.scrollTo(0, 0);
 		};
 
 		getBalances();
@@ -313,7 +314,6 @@ function History({ hideBackButton }: { hideBackButton?: boolean }) {
 			<BackButton
 				onClick={() => {
 					if (type) {
-						window.scrollTo(0, 0);
 						if (category) {
 							setCategory(undefined);
 							return;
